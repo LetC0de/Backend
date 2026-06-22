836dedb8463e2fa64dbbd3e18ddf4cf7b92735b8
LetC0de <abhishekjiiofficial86@gmail.com>
2026-06-22 21:44:09 +0530

    feat: add testing post routes for creating posts and log request data

--- authentication/src/routes/auth.routes.js
+++ authentication/src/routes/auth.routes.js
@@ -6,13 +6,5 @@ const router = express.Router();
 
 router.post("/register", authController.registerUser)
 
-router.get("/test",(req,res) => {
-    console.log( "Cookies: ",req.cookies)
-    res.json({
-        message : "Test route",
-        cookies : req.cookies 
-    })
-})
-
 
 module.exports = router;
