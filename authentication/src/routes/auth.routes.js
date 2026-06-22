87b09fdc47f388464381f5cb0efaf5974821eca2
LetC0de <abhishekjiiofficial86@gmail.com>
2026-06-22 19:47:10 +0530

    feat: add test route to display cookies in response

--- authentication/src/routes/auth.routes.js
+++ authentication/src/routes/auth.routes.js
@@ -6,5 +6,13 @@ const router = express.Router();
 
 router.post("/register", authController.registerUser)
 
+router.get("/test",(req,res) => {
+    console.log( "Cookies: ",req.cookies)
+    res.json({
+        message : "Test route",
+        cookies : req.cookies 
+    })
+})
+
 
 module.exports = router;
