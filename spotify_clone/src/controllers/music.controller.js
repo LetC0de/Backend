258ccc759376cc7c2b6608cc5893033ea608ddee
LetC0de <abhishekjiiofficial86@gmail.com>
2026-06-23 19:36:09 +0530

    feat: add multer for file upload handling and update music creation logic and fixed decoded

--- spotify_clone/src/controllers/music.controller.js
+++ spotify_clone/src/controllers/music.controller.js
@@ -21,33 +21,37 @@ async function createMusic(req,res){
                 message : "You do not have access to create music"
             })
         }
+
+        const {title} = req.body;
+        const file = req.file;
+
+        const result = await uploadFile(file.buffer.toString("base64"))
+
+        const music = await musicModel.create({
+            uri: result.url,
+            title,
+            artist: decoded.id
+        })
+
+        res.status(201).json({
+            message : "Music created successfully",
+            music : {
+                id : music._id,
+                uri : music.uri,
+                title : music.title,
+                artist : music.artist
+            }
+        })
+
+
     } catch (err){
+        console.log(err)
+
         return res.status(401).json({
             message : "Unauthorized"
         })
     }
 
-
-    const {title} = req.body;
-    const file = req.file;
-
-    const result = await uploadFile(file.buffer.toString("base64"))
-
-    const music = await musicModel.create({
-        uri: result.url,
-        title,
-        artist: decoded.id
-    })
-
-    res.status(201).json({
-        message : "Music created successfully",
-        music : {
-            id : music._id,
-            uri : music.uri,
-            title : music.title,
-            artist : music.artist
-        }
-    })
 }   
 
 module.exports = { createMusic };
