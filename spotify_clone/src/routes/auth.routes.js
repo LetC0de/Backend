adc34f1bee7ac255ffe81b6058b1b8dd2d1ad21b
LetC0de <abhishekjiiofficial86@gmail.com>
2026-06-25 16:55:53 +0530

    feat: add logout functionality to clear user session

--- spotify_clone/src/routes/auth.routes.js
+++ spotify_clone/src/routes/auth.routes.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 
 
-router.post("/register", authController.registerUser)
+router.post("/register", authController.registerUser);
 
-router.post("/login", authController.loginUser)
+router.post("/login", authController.loginUser);
+
+router.post("/logout", authController.logoutUser);
 
 
 module.exports = router; 
